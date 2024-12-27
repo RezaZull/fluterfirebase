81dc8b7aff5cbc3622ffe5a9ea8cb6b3dbe87bb8
Reza Zul <rezazuliansyah.s@gmail.com>
2024-12-27 20:27:51 +0700

    fix tampilan homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,22 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter_firebase1/screens/homescreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
-  @override
-  State<MainApp> createState() => _MainApp();
-}
-
-class _MainApp extends State<MainApp> {
-  final Future<FirebaseApp> _firebaseApp =
-      Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  String isOnValue = "true";
-  String temperatureValue = "0";
-  String isOpenValue = "true";
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Smart Home',
+      title: "Smart Home",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      darkTheme: ThemeData.dark(),
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: const Center(
-              child: Text("Monitoring Smart Home"),
-            ),
-          ),
-          body: FutureBuilder(
-              future: _firebaseApp,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return const Text("Tidak koneksi ke Firebase");
-                } else if (snapshot.hasData) {
-                  return content();
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              })),
-    );
-  }
-
-  Widget content() {
-    DatabaseReference testRef = FirebaseDatabase.instance.ref();
-
-    testRef.child('data/isOn').onValue.listen((event) {
-      setState(() {
-        isOnValue = event.snapshot.value.toString();
-      });
-    });
-    testRef.child('data/temperature').onValue.listen((event) {
-      setState(() {
-        temperatureValue = event.snapshot.value.toString();
-      });
-    });
-    testRef.child('data/isOpen').onValue.listen((event) {
-      setState(() {
-        isOpenValue = event.snapshot.value.toString();
-      });
-    });
-    return Column(
-      children: [
-        const Center(
-          child: Text("Hallo Rafifah "),
-        ),
-        Center(
-          child: Text("Temperature $temperatureValue"),
-        ),
-        Center(
-          child: Text("Status Lampu $isOnValue"),
-        ),
-        Switch(
-          value: isOnValue == 'true',
-          onChanged: (bool value) {
-            testRef.child('data/isOn').set(value);
-          },
-        ),
-        Center(
-          child: Text("Status Jendela $isOpenValue"),
-        ),
-        Switch(
-            value: isOpenValue == 'true',
-            onChanged: (bool value) {
-              testRef.child('data/isOpen').set(value);
-            })
-      ],
+      home: const Homescreen(),
     );
   }
 }
---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,242 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+  @override
+  State<Homescreen> createState() => _Homescreen();
+}
+
+class _Homescreen extends State<Homescreen> {
+  final Future<FirebaseApp> _firebaseApp =
+      Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  String isOnValue = "true";
+  String temperatureValue = "0";
+  String isOpenValue = "true";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Center(
+          child: Text("Monitoring Smart Home"),
+        ),
+      ),
+      body: FutureBuilder(
+          future: _firebaseApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text("Tidak koneksi ke Firebase");
+            } else if (snapshot.hasData) {
+              return content();
+            } else {
+              return const CircularProgressIndicator();
+            }
+          }),
+    );
+  }
+
+  Widget content() {
+    DatabaseReference testRef = FirebaseDatabase.instance.ref();
+
+    testRef.child('data/isOn').onValue.listen((event) {
+      setState(() {
+        isOnValue = event.snapshot.value.toString();
+      });
+    });
+    testRef.child('data/temperature').onValue.listen((event) {
+      setState(() {
+        temperatureValue = event.snapshot.value.toString();
+      });
+    });
+    testRef.child('data/isOpen').onValue.listen((event) {
+      setState(() {
+        isOpenValue = event.snapshot.value.toString();
+      });
+    });
+
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        primary: false,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.blue, width: 4),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.lightBlue.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 1,
+                  offset: const Offset(2.0, 2.0), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Temperature',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/thermometer.png',
+                      height: 60,
+                      width: 60,
+                    ),
+                    const Text('30°C',
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue))
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.blue, width: 4),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.lightBlue.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 1,
+                  offset: const Offset(2.0, 2.0), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Humidity',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/HumidityLogo.png',
+                      height: 60,
+                      width: 60,
+                    ),
+                    const Text('30%',
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue))
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.blue, width: 4),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.lightBlue.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 1,
+                  offset: const Offset(2.0, 2.0), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Lamp',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/lightbulb.png',
+                      height: 60,
+                      width: 60,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+
+    // return Padding(
+    //   padding: const EdgeInsets.all(20),
+    //   child: Column(
+    //     children: [
+    //       Container(
+    //         color: Colors.blue,
+    //         child: const Row(
+    //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //           children: [
+    //             Text('Suhu'),
+    //             SizedBox(
+    //               width: 20,
+    //             ),
+    //             Text('kelembapan'),
+    //           ],
+    //         ),
+    //       ),
+    //       const Center(
+    //         child: Text("Hallo Rafifah "),
+    //       ),
+    //       Center(
+    //         child: Text("Temperature $temperatureValue"),
+    //       ),
+    //       Center(
+    //         child: Text("Status Lampu $isOnValue"),
+    //       ),
+    //       Switch(
+    //         value: isOnValue == 'true',
+    //         onChanged: (bool value) {
+    //           testRef.child('data/isOn').set(value);
+    //         },
+    //       ),
+    //       Center(
+    //         child: Text("Status Jendela $isOpenValue"),
+    //       ),
+    //       Switch(
+    //           value: isOpenValue == 'true',
+    //           onChanged: (bool value) {
+    //             testRef.child('data/isOpen').set(value);
+    //           }),
+    //     ],
+    //   ),
+    // );
+  }
+}
